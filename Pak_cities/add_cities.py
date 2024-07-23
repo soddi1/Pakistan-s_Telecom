ce8a8f7d1cb901e8b3a30c05e274b5327768910a
soddi1 <25100006@lums.edu.pk>
2024-07-23 18:11:00 +0500

    added cities

--- Pak_cities/add_cities.py
+++ Pak_cities/add_cities.py
@@ -1,7 +1,7 @@
 import json
 
 input_file = 'pak_city_names.json'
-output_file = 'output.json'
+output_file = 'pak_city_names.json'
 
 def read_json(file_path):
     with open(file_path, 'r', encoding='utf-8') as infile:
